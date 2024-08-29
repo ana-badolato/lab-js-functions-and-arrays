bc17f034f4304cdaec76e6cdeff6e4cfceb02618
Ana Badolato <badolatoana@gmail.com>
2024-08-29 15:59:05 +0200

    lab done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,17 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+
+    if (num1>num2){
+        return num1;
+    }else if (num1<num2){
+        return num2;
+    }else if (num1===num2){
+        return num1;
+    }else {
+        return "oops, something went wrong"
+    }
+
+}
 
 
 
@@ -7,15 +19,45 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+let maxCharFound="";
+
+function findLongestWord(arrWords) {
+
+    if (arrWords.length === 0) {
+        return null;
+    }
+    
+    maxCharFound = arrWords[0];
+    
+    for (let i = 1; i < arrWords.length; i++) {
+        if (arrWords[i].length > maxCharFound.length) {
+            maxCharFound = arrWords[i];
+        }
+    }
+    
+    return maxCharFound;
+}
 
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+let sum;
 
-function sumNumbers() {}
+function sumNumbers(arrNumbers) {
+    if (arrNumbers.length === 0) {
+        return 0;
+    }
+    
+    sum = arrNumbers[0];
+
+    for (let i = 1; i < arrNumbers.length; i++) {
+        sum += arrNumbers[i];
+    }
+
+    return sum;
+}
 
 
 
@@ -23,7 +65,16 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrNumbersAvg) {
+
+    if (arrNumbersAvg.length === 0) {
+        return 0;
+    }
+
+    let sumAllNumbers = sumNumbers(arrNumbersAvg);
+    return sumAllNumbers / arrNumbersAvg.length
+
+}
 
 
 
@@ -31,4 +82,17 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrWords, word) {
+    console.log(arrWords);
+    console.log(word);
+
+    if (arrWords.length === 0) {
+        return null;
+    }
+
+    if(arrWords.includes(word)){
+        return true;
+    }
+
+    return false;
+}
